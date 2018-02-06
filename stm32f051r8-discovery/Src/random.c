3bd13bc3d0c458b36a2ef542dcf353b316fb0764
Marijan Kostrun <mkostrun@gmail.com>
2018-02-06 11:16:12 -0500

    Add files via upload

---
+++ stm32f051r8-discovery/Src/random.c
@@ -0,0 +1,155 @@
+#include "main.h"
+
+
+#undef CRC_SALT_I
+#undef CRC_SALT_F
+
+// #define  CRC_SALT_I   0xBADA55E5
+// #define  CRC_SALT_F   0xBADA55E5
+
+#if defined(USE_STM32F0XX_NUCLEO) || defined(USE_STM32F4XX_NUCLEO) || defined(USE_STM32F0XX_DISCOVERY)
+
+#ifdef USE_STM32F0XX_NUCLEO
+#include "stm32f0xx_hal.h"
+#include "stm32f0xx_hal_conf.h"
+#include "stm32f0xx_hal_adc.h"
+#if defined(CRC_SALT_I)
+#include "stm32f0xx_hal_crc.h"
+#include "stm32f0xx_hal_crc_ex.h"
+#endif
+#endif
+
+#ifdef USE_STM32F0XX_DISCOVERY
+#include "stm32f0xx_hal.h"
+#include "stm32f0xx_hal_conf.h"
+#include "stm32f0xx_hal_adc.h"
+#if defined(CRC_SALT_I)
+#include "stm32f0xx_hal_crc.h"
+#include "stm32f0xx_hal_crc_ex.h"
+#endif
+#endif
+
+#ifdef USE_STM32F4XX_NUCLEO
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_conf.h"
+#include "stm32f4xx_hal_adc.h"
+#if defined(CRC_SALT_I)
+  #include "stm32f4xx_hal_crc.h"
+  #include "stm32f4xx_hal_crc_ex.h"
+#endif
+#endif
+
+static uint8_t init_trn=0;
+ADC_HandleTypeDef hadc;
+  #if defined(CRC_SALT_I)
+  CRC_HandleTypeDef hcrc;
+  #endif
+
+static void open_trand_crc(void)
+{
+  __HAL_RCC_ADC1_CLK_ENABLE();
+
+  // Initialize ADC 14MHz RC
+  __HAL_RCC_HSI14_ENABLE();
+  __HAL_RCC_HSI14ADC_ENABLE();
+  while (!__HAL_RCC_GET_FLAG(RCC_FLAG_HSI14RDY));
+  /**
+   * Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+   */
+  ADC_ChannelConfTypeDef sConfig;
+  hadc.Instance = ADC1;
+  hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_BACKWARD;
+  hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc.Init.LowPowerAutoWait = DISABLE;
+  hadc.Init.LowPowerAutoPowerOff = DISABLE;
+  hadc.Init.ContinuousConvMode = DISABLE;
+  hadc.Init.DiscontinuousConvMode = DISABLE;
+  hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc.Init.DMAContinuousRequests = DISABLE;
+  hadc.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  if (HAL_ADC_Init(&hadc) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  /**
+   * Configure for the selected ADC regular channel to be converted.
+   */
+  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  /**
+   * Enable CRC clock
+   */
+  #if defined(CRC_SALT_I)
+  __HAL_RCC_CRC_CLK_ENABLE();
+  hcrc.Instance = CRC;
+  hcrc.Init.CRCLength = CRC_POLYLENGTH_32B;
+  hcrc.Init.DefaultInitValueUse = DEFAULT_INIT_VALUE_DISABLE;
+  hcrc.Init.InitValue = CRC_SALT_I;
+  hcrc.Init.DefaultPolynomialUse = DEFAULT_POLYNOMIAL_ENABLE;
+  hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_NONE;
+  hcrc.Init.OutputDataInversionMode = CRC_OUTPUTDATA_INVERSION_DISABLE;
+  hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_WORDS;
+  if (HAL_CRC_Init(&hcrc) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+  #endif
+}
+
+static void close_trand_crc(void)
+{
+  /* disable non-needed services */
+  __HAL_RCC_ADC1_CLK_DISABLE();
+  #if defined(CRC_SALT_I)
+  __HAL_RCC_CRC_CLK_DISABLE();
+  #endif
+}
+
+uint32_t RandomUInt32(uint8_t size)
+{
+  uint32_t val=0;
+
+  if (!init_trn)
+    open_trand_crc();
+
+  for (uint8_t k=0; k<4; k++)
+  for (uint8_t i=0; i<(size>>1); i++)
+  {
+    /* Start ADC1 Software Conversion */
+    if (HAL_ADC_Start(&hadc) != HAL_OK)
+    {
+      _Error_Handler(__FILE__, __LINE__);
+    }
+
+    /* wait for conversion complete */
+    while (!__HAL_ADC_GET_FLAG(&hadc, ADC_FLAG_EOC));
+
+    /* Two LS bits are most likely most random */
+    val ^= (HAL_ADC_GetValue(&hadc) & 0x00000003) << (2*i);
+
+    //clear EOC flag
+    __HAL_ADC_CLEAR_FLAG(&hadc, ADC_FLAG_EOC);
+  }
+  #if defined(CRC_SALT_I)
+  val = HAL_CRC_Accumulate(&hcrc, &val, 1);
+  /* Add salt */
+  val = CRC_SALT_F;
+  val = HAL_CRC_Accumulate(&hcrc, &val, 1);
+  #endif
+  return val;
+}
+
+#endif
+
+ 
---
+++ stm32f051r8-discovery/Src/usart.c
@@ -0,0 +1,214 @@
+/**
+  ******************************************************************************
+  * File Name          : USART.c
+  * Description        : This file provides code for the configuration
+  *                      of the USART instances.
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN 0 */
+#define SERIAL_BAUDRATE     (115200)
+#define SERIAL_WAITTIME_MS  (20)
+/* USER CODE END 0 */
+
+UART_HandleTypeDef huart2;
+__IO ITStatus UART_TX_Completed=RESET, UART_RX_Available=RESET;
+
+void print_serial(char * msg);
+
+void MX_UART2_Configuration(void)
+{
+  huart2.Instance        = USART2;
+  huart2.Init.BaudRate   = SERIAL_BAUDRATE;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits   = UART_STOPBITS_1;
+  huart2.Init.Parity     = UART_PARITY_NONE;
+  huart2.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
+  huart2.Init.Mode       = UART_MODE_TX_RX;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  __HAL_LOCK(&huart2); /* Process Locked */
+  huart2.rx_last = huart2.rx_first = huart2.rx_available = 0;
+  huart2.ErrorCode = HAL_UART_ERROR_NONE;
+  huart2.RxState = HAL_UART_STATE_BUSY_RX;
+  __HAL_UNLOCK(&huart2);  /* Process Unlocked */
+
+  /* Enable the UART Error Interrupt: (Frame error, noise error, overrun error) */
+  SET_BIT(huart2.Instance->CR3, USART_CR3_EIE);
+  /* Enable the UART Parity Error and Data Register not empty Interrupts */
+  SET_BIT(huart2.Instance->CR1, USART_CR1_PEIE | USART_CR1_RXNEIE);
+
+  /* USART2 interrupt Init */
+  HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(USART2_IRQn);
+
+  HAL_Delay(50);
+  print_serial("\n");
+}
+
+/**
+ * @brief  Initializes UART MSP.
+ * @param  UART_HandleTypeDef* pUARTHandle
+ * @retval None
+ */
+void HAL_UART_MspInit(UART_HandleTypeDef* pUARTHandle)
+{
+  if (pUARTHandle->Instance == USART2)
+  {
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    /* USER CODE BEGIN USART2_MspInit 1 */
+    /*##-1- Enable peripherals and GPIO Clocks #################################*/
+    /* Enable GPIO TX/RX clock */
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    /* Enable USART2 clock */
+    __HAL_RCC_USART2_CLK_ENABLE();
+
+    /*##-2- Configure peripheral GPIO ##########################################*/
+    /* UART TX GPIO pin configuration  */
+    GPIO_InitStruct.Pin       = GPIO_PIN_2;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull      = GPIO_NOPULL;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF1_USART2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* UART RX GPIO pin configuration  */
+    GPIO_InitStruct.Pin = GPIO_PIN_3;
+    GPIO_InitStruct.Alternate = GPIO_AF1_USART2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    HAL_Delay(50);
+  }
+}
+
+
+uint8_t UART2_Transmit(uint8_t * msg, uint16_t lenmsg)
+{
+  UART_TX_Completed = RESET;
+  return HAL_UART_Transmit_IT(&huart2, msg, lenmsg);
+}
+
+/* USER CODE BEGIN 1 */
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *UartHandle)
+{
+  /* Set transmission flag: trasfer complete*/
+  UART_TX_Completed = SET;
+}
+
+/******************************************************************************/
+/* STM32F0xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32f0xx.s).                    */
+/******************************************************************************/
+/* USER CODE BEGIN 1 */
+void USART2_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART2_IRQn 0 */
+
+  /* USER CODE END USART2_IRQn 0 */
+  HAL_UART_Cir_IRQHandler(&huart2);
+  /* USER CODE BEGIN USART2_IRQn 1 */
+
+  /* USER CODE END USART2_IRQn 1 */
+}
+
+
+void print_serial(char * msg)
+{
+  UART2_Transmit((uint8_t*) msg, strlen(msg));
+  while (UART_TX_Completed==RESET);
+}
+
+uint8_t serial_input_available(void)
+{
+  uint8_t n = HAL_UART_Cir_Available_IT(&huart2);
+
+  if (!n)
+    return 0;
+
+  if (HAL_UART_Cir_Last_Time_IT(&huart2)<SERIAL_WAITTIME_MS)
+    return 0;
+
+  return n;
+}
+
+uint8_t serial_input (char * buffer, uint8_t len)
+{
+  uint8_t n = serial_input_available();
+  if (!n)
+    return 0;
+
+  uint16_t i = 0;
+  while (HAL_UART_Cir_Available_IT(&huart2))
+  {
+    buffer[i] = HAL_UART_Cir_RetrieveReceivedChar_IT(&huart2);
+    i++;
+
+    if (i==len)
+      break;
+  }
+
+  while (buffer[i-1]=='\n')
+    i--;
+  while (buffer[i-1]=='\r')
+    i--;
+  buffer[i] = '\0';
+
+  return i;
+}
+
+
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
