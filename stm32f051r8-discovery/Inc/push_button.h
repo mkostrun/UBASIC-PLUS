50160e4719f919edf0e00a3893287ceaa351ba58
Marijan Kostrun <mkostrun@gmail.com>
2018-02-06 11:17:23 -0500

    Add files via upload

---
+++ stm32f051r8-discovery/Inc/push_button.h
@@ -0,0 +1,7 @@
+#ifndef __PUSH_BUTTON_H
+#define __PUSH_BUTTON_H
+
+extern __IO uint8_t pb_status, hw_event;
+void  PB_Init(void);
+
+#endif
---
+++ stm32f051r8-discovery/Inc/random.h
@@ -0,0 +1,6 @@
+#ifndef __RANDOM_H
+#define __RANDOM_H
+
+uint32_t RandomUInt32(uint8_t size);
+
+#endif
